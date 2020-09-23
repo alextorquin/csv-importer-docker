347862010d9308acb352f35e19170350ed797cd5
James Cole <thegrumpydictator@gmail.com>
2020-09-23 06:34:50 +0200

    New build script.

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM jc5x/firefly-iii-base-image:latest
+
+# To learn more about this base image, visit https://github.com/firefly-iii/docker-base-image
+
+ARG version
+ENV VERSION=$version
+LABEL version="1.1" maintainer="thegrumpydictator@gmail.com"
+
+RUN curl -SL https://github.com/firefly-iii/csv-importer/archive/$VERSION.tar.gz | tar xzC $FIREFLY_III_PATH --strip-components 1 && \
+    chmod -R 775 $FIREFLY_III_PATH/storage && \
+    composer install --prefer-dist --no-dev --no-scripts --no-suggest && /usr/local/bin/finalize-image.sh
+
+ENTRYPOINT ["/usr/local/bin/entrypoint.sh"]
---
+++ build-travis.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+#
+# Step 1: set repos name.
+#
+#REPOS_NAME=jc5x/test-repository
+#REPOS_NAME=jc5x/firefly-iii
+REPOS_NAME=fireflyiii/csv-importer
+
+#
+# Step 2: echo some info
+#
+echo "build-travis.sh v1.0: I am building '${VERSION}' for ${REPOS_NAME}."
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+# new script start
+
+echo "Current directory is $DIR"
+
+docker version -f '{{.Server.Experimental}}'
+docker version
+
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes i
+docker buildx create --name firefly_iii_builder
+docker buildx inspect firefly_iii_builder --bootstrap
+docker buildx use firefly_iii_builder
+
+# always also push same label as the version (ie develop)
+LABEL=$VERSION
+
+# if the version is an alpha version, push to "alpha":
+if [[ $VERSION == *"alpha"* ]]; then
+	LABEL="alpha"
+fi
+
+# if the version is a beta version, push to "beta":
+if [[ $VERSION == *"beta"* ]]; then
+	LABEL="beta"
+fi
+
+if [[ $VERSION != *"beta"* && $VERSION != *"alpha"* && $VERSION != *"develop"* ]]; then
+	LABEL="latest"
+fi
+
+echo "Version is '$VERSION' so label will be '$LABEL'."
+
+# build firefly iii (TODO)
+#docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 --build-arg version=$VERSION -t $REPOS_NAME:$PUSHVERSION --push . -f Dockerfile --buildarg 
+docker buildx build  --build-arg version=$VERSION --platform linux/amd64,linux/arm64,linux/arm/v7 -t $REPOS_NAME:$LABEL --push . -f Dockerfile
+
+echo "Done!"
